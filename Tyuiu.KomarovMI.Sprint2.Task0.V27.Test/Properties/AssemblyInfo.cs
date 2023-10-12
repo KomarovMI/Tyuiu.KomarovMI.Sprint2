ece64b2763adb53f31a6876dd32944d49e28ba08
Tyuiu.KomarovMI@outlook.com <maximpypsik@mail.ru>
2023-10-12 20:09:29 +0500

    Add project files.

---
+++ Tyuiu.KomarovMI.Sprint2.Task0.V27.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KomarovMI.Sprint2.Task0.V27.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KomarovMI.Sprint2.Task0.V27.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1274be5e-953f-4905-a978-b9e86c84528e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.KomarovMI.Sprint2.Task0.V27/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KomarovMI.Sprint2.Task0.V27.Lib;
+
+
+namespace Tyuiu.KomarovMI.Sprint2.Task0.V27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2 | Выполнил: Комаров М.И. | ПКТБ-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*Спринт #2                                                                *");
+            Console.WriteLine("* Тема:                                                                   *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #27                                                             *");
+            Console.WriteLine("* Выполнил: Комаров Максим Игоревич | ПКТБ-23-1                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу из операций сравнений (==, !=, <, >, <=, >=          *");
+            Console.WriteLine("* и арифметических выражений, которая вернет логическую последовательность*");
+            Console.WriteLine("* (True, False, True, False, False, True)                                 *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int x = 1305;
+            int y = 275;
+            bool[] res = new bool[6];
+            res = ds.GetCompareOperations(x, y);
+            Console.WriteLine("X=" + x);
+            Console.WriteLine("Y=" + y);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
