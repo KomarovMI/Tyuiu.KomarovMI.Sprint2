e3bba330aa176ca2793d43b9bae91952e6ca4412
Tyuiu.KomarovMI@outlook.com <maximpypsik@mail.ru>
2023-10-14 23:51:55 +0500

    Task3

---
+++ Tyuiu.KomarovMI.Sprint2.Task3.V5.Test/DataServiceTest.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.KomarovMI.Sprint2.Task3.V5.Lib;
+
+namespace Tyuiu.KomarovMI.Sprint2.Task3.V5.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double res = ds.Calculate(x);
+            double wait = -7;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 0;
+            double res = ds.Calculate(x);
+            double wait = -0.083;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void ValidCondition3()
+        {
+            DataService ds = new DataService();
+            double x = -2;
+            double res = ds.Calculate(x);
+            double wait = 0.02;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void ValidCondition4()
+        {
+            DataService ds = new DataService();
+            double x = -10;
+            double res = ds.Calculate(x);
+            double wait = -1100.01;
+            Assert.AreEqual(wait, res);
+        }
+
+    }
+}
---
+++ Tyuiu.KomarovMI.Sprint2.Task3.V5/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KomarovMI.Sprint2.Task3.V5.Lib;
+
+namespace Tyuiu.KomarovMI.Sprint2.Task3.V5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2 | Выполнил: Комаров М.И. | ПКТБ-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*Спринт #2                                                                *");
+            Console.WriteLine("* Тема:                                                                   *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #5                                                              *");
+            Console.WriteLine("* Выполнил: Комаров Максим Игоревич | ПКТБ-23-1                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет требуемое значение функции Y с    *");
+            Console.WriteLine("* использованием вложенных оператор if-else, где пользователь вводит      *");
+            Console.WriteLine("* значение переменной X с клавиатуры.                                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите значение Х:");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double res = ds.Calculate(x);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Значение Y=" + res);
+            Console.ReadKey();
+
+        }
+    }
+}
